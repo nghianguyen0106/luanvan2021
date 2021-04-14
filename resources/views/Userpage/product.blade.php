f7c7c78e7567c5656f9881a6d4e14dae0e8060c5
Nghia <nguyenchinghia199916@gmail.com>
2021-04-15 00:05:31 +0700

    14_4_nghia_productpage

--- resources/views/Userpage/product.blade.php
+++ resources/views/Userpage/product.blade.php
@@ -95,7 +95,7 @@ Product
 
 										<div class="men-cart-pro">
 											<div class="inner-men-cart-pro">
-												<a href="{{URL::to('proinfo/'.$i->spMa)}}" class="link-product-add-cart">Quick Viewx</a>
+												<a href="{{URL::to('proinfo/'.$i->spMa)}}" class="link-product-add-cart">Quick Views</a>
 											</div>
 										</div>
 										{{-- <span class="product-new-top">New</span> --}}
