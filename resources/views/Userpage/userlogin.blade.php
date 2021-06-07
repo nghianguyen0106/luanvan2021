17304d26560fa9271310998621a566210ab7548c
ltnhan2208 <letrungnhan99@gmail.com>
2021-06-07 14:22:31 +0700

    up truoc r sua lai

--- resources/views/Userpage/userlogin.blade.php
+++ resources/views/Userpage/userlogin.blade.php
@@ -1,42 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="{{{'public/fe/login/fonts/icomoon/style.css'}}}">
-
-    <link rel="stylesheet" href="{{{'public/fe/login/css/owl.carousel.min.css'}}}">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{{'public/fe/login/css/bootstrap.min.css'}}}">
-    
-    <!-- Style -->
-    <link rel="stylesheet" href="{{{'public/fe/login/css/style.css'}}}">
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-{{-- FontAW --}}
-<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-    <title>Đăng nhập</title>
-  </head>
-  <body>
-  
-
-  <div class="d-lg-flex half">
-    <div class="bg order-1 order-md-2" style="background-image: url('{{{'public/fe/login/images/bg_2.jpg'}}}');"></div>
-    <div class="contents order-2 order-md-1">
-
-      <div class="container">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-md-7">
-            <div class="mb-4">
-              <h3>Đăng nhập</h3>
-              <p style="color: red" class="mb-4">
-                </p>
-            </div>
-            <form action="{{URL::to('/checklogin')}}" method="post">
+@extends("Userpage.layout_dn_dk")
+@section('title')
+  Đăng nhập
+@endsection
+@section("content")
+<br/>
+  <section class="container content__login">
+    <div class="row">
+      <div class="col-lg-3 col-sm-1"></div>
+      <div class="col-lg-6 col-sm-12">
+          <form action="{{URL::to('/checklogin')}}" method="post">
+              <legend>Đăng nhập</legend>
             {{csrf_field()}}
+            <div class="content__login--form">
               <div class="form-group first">
                 <label for="username">Tên đăng nhập</label>
                 <input type="text" name="username" class="form-control" id="username">
@@ -44,10 +19,9 @@
               </div>
               <div class="form-group last mb-3">
                 <label for="password">Mật khẩu</label>
-                <input type="password" name="password" class="form-control" id="password">
-                 <i id="show__pass1" class="far fa-eye" style="font-size: 23px;position: absolute;top:1.5rem;left:90%;"></i>
+                <input id="password" type="password" name="password" class="form-control">
+                <i id="show__pass1" class="far fa-eye" style="font-size: 23px;"></i>
               </div>
-
               
               <div class="d-flex mb-5 align-items-center">
                 <label class="control control--checkbox mb-0"><span class="caption">Nhớ tài khoản</span>
@@ -57,8 +31,7 @@
                 <span class="ml-auto"><a href="{{URL::to('forgotPassword')}}" class="forgot-pass">Quên mật khẩu</a></span> 
               </div>
 
-              <input type="submit" value="Log In" class="btn btn-block btn-primary">
-
+              <input type="submit" value="Đăng nhập" class="btn btn-block btn-primary">
 
               <span class="d-block text-center my-4 text-muted">&mdash; Hoặc &mdash;</span>
               <div class="row" style="color: white; ">
@@ -69,28 +42,19 @@
                                             <i class="fab fa-facebook-f fa-fw"></i> Đăng nhập bằng <strong>Facebook</strong>
                                         </a>
               </div>
-                
-                  <span class="d-block text-center my-4 text-muted"><a  style="text-decoration: none;" href="{{URL::to('/')}}">Trở về trang chính</a>&nbsp;/&nbsp;<a style="text-decoration: none;"  href="{{URL::to('/register')}}">Đăng ký</a></span>
-
-            </form>
-          </div>
+              <br/>
+              <div class="content__login--link">
+                    <a href="{{URL::to('/')}}">Trở về trang chính</a>&nbsp;/&nbsp;<a href="{{URL::to('/register')}}">Đăng ký</a>
+              </div>
+              <br/>
+              </div>
+          </form>
         </div>
+        <div class="col-lg-3 col-sm-1"></div>
       </div>
-    </div>
-
-    
-  </div>
-    
-    
-
-    <script src="{{{'public/fe/login/js/jquery-3.3.1.min.js'}}}"></script>
-    <script src="{{{'public/fe/login/js/popper.min.js'}}}"></script>
-    <script src="{{{'public/fe/login/js/bootstrap.min.js'}}}"></script>
-    <script src="{{{'public/fe/login/js/main.js'}}}"></script>
-    <script src="{{{'public/fe/login/js/js.js'}}}"></script>
-
-  </body>
-</html>
+  </section>
+  <br/>
+<!--Javascript code-->
   @if(Session::has('loginmessage'))
  <script type="text/javascript" >
 Swal.fire({
@@ -121,3 +85,5 @@ Swal.fire({
 })
 </script> 
 @endif
+
+@endsection
--- resources/views/Userpage/userregister.blade.php
+++ resources/views/Userpage/userregister.blade.php
@@ -1,38 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{url('public/fe/login/fonts/icomoon/style.css')}}">
-    <link rel="stylesheet" href="{{url('public/fe/login/css/owl.carousel.min.css')}}">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{url('public/fe/login/css/bootstrap.min.css')}}">
-    <!-- Style -->
-    <link rel="stylesheet" href="{{url('public/fe/login/css/style.css')}}">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-    <title>Đăng ký tài khoản</title>
-  </head>
-  <body>
-  
-
-  <div class="d-lg-flex half">
-    <div class="bg order-1 order-md-2" style="background-image: url('{{{'public/fe/login/images/bg_2.jpg'}}}');"></div>
-    <div class="contents order-2 order-md-1">
-
-      <div class="container">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-md-7">
-            <div class="mb-4">
-              <h3>Đăng ký tài khoản</h3>
-              <p style="color: red" class="mb-4">
-                </p>
-            </div>
+@extends("Userpage.layout_dn_dk")
+@section('title')
+  Đăng ký tài khoản
+@endsection
+@section("content")
+<br/>
+<section class="container content__register">
+    <div class="row">
+      <div class="col-lg-3 col-sm-1"></div>
+      <div class="col-lg-6 col-sm-12">
             <form action="{{URL::to('getregister')}}" method="post">
+             <legend>Đăng ký tài khoản</legend>
             {{csrf_field()}}
+            <div class="content__register--form">
                <div class="form-group first">
                 <label for="name">Tên của bạn</label>
                 <input type="text" name="name" required class="form-control" >
@@ -51,18 +30,18 @@
               </div>
                <div class="form-group ">
                 <label for="password">Mật khẩu</label>
-                <input id="password" type="password" name="password" required class="form-control" >
-                <i id="show__pass1" class="far fa-eye" style="font-size: 23px;position: absolute;top:1.5rem;left:90%;"></i>
+                <input id="password" type="password" name="password" required class="form-control">
+                <i id="show__pass1" class="far fa-eye" style="font-size: 23px;"></i>
               </div>
               <div class="form-group  ">
-                <label for="repassword">Nhập lại mật khẩu</label>
+                <label for="password">Nhập lại mật khẩu</label>
                 <input id="repassword" type="password" name="repassword" required class="form-control">
-                 <i id="show__pass2" class="far fa-eye" style="font-size: 23px;position: absolute;top:1.5rem;left:90%;"></i>
+                 <i id="show__pass2" class="far fa-eye" style="font-size: 23px;"></i>
               </div>
               
               <div class="form-group  ">
                 <label for="address">Địa chỉ</label>
-                <input type="text" name="address" required class="form-control" >
+                <input type="text" name="address" required class="form-control">
               </div>
               <div class="form-group  ">
                 <label for="sdt">Số điện thoại</label>
@@ -74,10 +53,9 @@
               </div>
 
 
-              <input type="submit" value="Register" class="btn btn-block btn-primary">
-            
-            </form>
-              <span class="d-block text-center my-4 text-muted">&mdash; Hoặc &mdash;</span>
+              <input type="submit" value="Đăng ký" class="btn btn-block btn-primary">
+              <br/>
+               <span class="d-block text-center my-4 text-muted">&mdash; Hoặc &mdash;</span>
               <div class="row" style="color: white; ">
                    <a href="{{URL::to('/google')}}" style="text-decoration: none;" class="btn btn-outline-danger btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Đăng ký bằng <strong>Google</strong>
@@ -86,27 +64,18 @@
                                             <i class="fab fa-facebook-f fa-fw"></i> Đăng ký bằng <strong>Facebook</strong>
                                         </a>
               </div>
-                  <span class="d-block text-center my-4 text-muted"><a href="{{URL::to('/')}}">Trở về trang chính</a>&nbsp;/&nbsp;<a href="{{URL::to('login')}}">Đã có tài khoản <strong>Đăng nhập ngay </strong>?</a></span>
+                  <div class="content__register--link">
+                    <a href="{{URL::to('/')}}">Trở về trang chính</a>&nbsp;/&nbsp;
+                    <a href="{{URL::to('login')}}">Đã có tài khoản <strong>Đăng nhập ngay </strong>?</a>
+                  </div>
+                </div>
+            </form>
           </div>
         </div>
-      </div>
-    </div>
-
-    
-  </div>
-    
-    
-
-    <script src="{{url('public/fe/login/js/jquery-3.3.1.min.js')}}"></script>
-    <script src="{{url('public/fe/login/js/popper.min.js')}}"></script>
-    <script src="{{url('public/fe/login/js/bootstrap.min.js')}}"></script>
-    <script src="{{url('public/fe/login/js/main.js')}}"></script>
-     <script src="{{url('public/fe/login/js/js.js')}}"></script>
-  </body>
-</html>
-
-  @if(Session::has('error'))
- <script type="text/javascript" >
+        </section>
+<!----Javascript code-->
+@if(Session::has('error'))
+<script type="text/javascript" >
 Swal.fire({
   icon: 'error',
   title: 'Opss... ',
@@ -114,3 +83,7 @@ Swal.fire({
 })
 </script> 
 @endif
+
+
+
+@endsection
