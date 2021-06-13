acf9b77080e67fa52153831a4fcd90aeb1ba647e
Nghia <nguyenchinghia199916@gmail.com>
2021-06-13 15:40:51 +0700

    13_06_nghia_sửa lỗi wishlish & slide table

--- database/migrations/2021_05_11_212039_create_slide_table.php
+++ database/migrations/2021_05_11_212039_create_slide_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBannerTable extends Migration
+class CreateSlideTable extends Migration
 {
     /**
      * Run the migrations.
