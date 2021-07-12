4be5c1893f2afbd70c8ed3bce6c9f11e46daca11
Nghia <nguyenchinghia199916@gmail.com>
2021-07-12 09:11:01 +0700

    12_07_nghia_ sua db serial

--- database/migrations/2021_07_12_050404_create_serial_table.php
+++ database/migrations/2021_07_12_050404_create_serial_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateImeisanphamTable extends Migration
+class CreateSerialTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateImeisanphamTable extends Migration
      */
     public function up()
     {
-        Schema::create('imeisanpham', function (Blueprint $table) {
+        Schema::create('serial', function (Blueprint $table) {
             $table->integer('spMa');
-            $table->char('imeiMa',20);
-            $table->integer('imeiTinhtrang');
+            $table->char('serMa',20);
+            $table->integer('serTinhtrang');
             $table->engine="InnoDB";
 
             $table->foreign('spMa')->references('spMa')->on('sanpham')->onDelete('cascade')->onUpdate('cascade');
@@ -30,6 +30,6 @@ class CreateImeisanphamTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('imeisanpham');
+        Schema::dropIfExists('serial');
     }
 }
